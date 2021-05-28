dcc7133e99690039ed9cfbccfc4b1ad595a9bd8d
AVoss <a.vosseler@gmx.net>
2021-05-28 20:59:01 +0200

    Update bocd.py

--- bocd.py
+++ bocd.py
@@ -3,7 +3,6 @@ from scipy.stats import norm
 from scipy.special import logsumexp
 from tqdm import tqdm
 
-
 class BayesOCPD(BaseEstimator, OutlierMixin):
     """
     Bayesian online change point detection, see Adams & MacKay 2007.
@@ -20,7 +19,6 @@ class BayesOCPD(BaseEstimator, OutlierMixin):
         Return run length posterior using Algorithm 1
         """
         data = deepcopy(X)
-        # 1. Initialize
         T           = len(data)
         log_R       = -np.inf * np.ones((T+1, T+1))    # log posterior values of run length at time t
         log_R[0, 0] = 0              # log 0 == 1
@@ -32,36 +30,35 @@ class BayesOCPD(BaseEstimator, OutlierMixin):
         cps_MAP     = np.empty(T) 
 
         for t in tqdm(range(1, T+1)):       # forward filtering 
-            # 2. Observe new datum.
+            # Observe new datum.
             x = data[t-1]
 
             # Make model predictions. ('step 9'). Calculate first two moments
             pmean[t-1] = np.sum(np.exp(log_R[t-1, :t]) * self.model.mean_params[:t])   # up to t-1
             pvar[t-1]  = np.sum(np.exp(log_R[t-1, :t]) * self.model.var_params[:t])
             
-            # 3. Evaluate predictive posterior probabilities.
+            # Evaluate predictive posterior probabilities.
             log_pis = model.log_pred_prob(t, x)
 
-            # 4. Calculate growth probabilities ('No break')
+            # Calculate growth probabilities ('No break')
             log_growth_probs = log_pis + log_message + log_1mH
 
-            # 5. Calculate changepoint/Break probabilities.
+            # Calculate changepoint/Break probabilities.
             log_cp_prob = logsumexp(log_pis + log_message + log_H)
 
-            # 6. Calculate evidence
+            # Calculate evidence
             new_log_joint = np.append(log_cp_prob, log_growth_probs)    # append: pr(r_t == 0), pr(r_t > 0)'s
 
-            # 7. Determine run length posterior mass distribution.
+            # Determine run length posterior mass distribution.
             log_R[t, :t+1]  = new_log_joint
             log_R[t, :t+1] -= logsumexp(new_log_joint)            # normalize entries by dividing by column sum
 
-            # 8. Update sufficient statistics.
+            # Update sufficient statistics.
             # here: mean_params & prec_params
             model.update_params(t, x)
 
             # Pass message.
             log_message = new_log_joint
-
             # Select break points, via MAP state estimate        
             cps_MAP[t-1] = np.argmax(log_R[t-1,:t])
         
